41724605540559f379cfc1bc9e91be1f15cddde0
WardaRafique <124115849+WardaRafique@users.noreply.github.com>
2023-02-22 20:13:24 +0500

    Update Calculator using switch statement.cpp

--- Calculator using switch statement.cpp
+++ Calculator using switch statement.cpp
@@ -1,15 +1,23 @@
 #include <iostream>
+//preprocessor directive
 using namespace std;
 
-int main() {
+int main()
+main function
+{
     double num1, num2, result;
+    declaration of variables
     char op;
 
     cout << "Enter the first number: ";
+    //display the first variable
     cin >> num1;
+    take input from user
 
     cout << "Enter an operator (+, -, *, /): ";
+    //display the operators
     cin >> op;
+    //take input from user
 
     cout << "Enter the second number: ";
     cin >> num2;
@@ -17,6 +25,7 @@ int main() {
     switch(op) {
         case '+':
             result = num1 + num2;
+            //display it ,if the case is true
             break;
 
         case '-':
@@ -33,11 +42,13 @@ int main() {
     
 
         default:
+        //display it ,if all the above cases becomes false
             cout << "Invalid operator";
             return 0;
     }
 
     cout << num1 << " " << op << " " << num2 << " = " << result;
+    //display the statement
 
     return 0;
 }
